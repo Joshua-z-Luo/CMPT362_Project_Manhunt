e69a9b5e14037585fc705b1e02b63eefa6bfea7b
forestoftrees <forestysh@gmail.com>
2025-12-02 06:54:03 -0800

    deleted extra folder

--- CMPT362_Manhunt/app/src/main/java/joshua_luo/example/cmpt362projectmanhunt/ProfileSettingsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package joshua_luo.example.cmpt362projectmanhunt
-
-import android.graphics.Bitmap
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class ProfileSettingsViewModel: ViewModel() {
-    val userImage = MutableLiveData<Bitmap>()
-}
--- CMPT362_Manhunt/app/src/main/java/joshua_luo/example/cmpt362projectmanhunt/model/Point.kt
+++
@@ -1,9 +0,0 @@
-package joshua_luo.example.cmpt362projectmanhunt.model
-
-/**
- * Simple latitude/longitude point
- */
-data class Point(
-    val latitude: Double,
-    val longitude: Double
-)
